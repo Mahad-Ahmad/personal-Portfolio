ecf88c7492a92c50e5831469a41b057740637798
Mahad Ahmad <mahadahmad921@gmail.com>
2024-04-12 01:24:15 +0500

    update: projects page updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,98 +1,147 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import hsp from "../assets/img/hsp.png";
+import nabr from "../assets/img/nabr.png";
+import fbr from "../assets/img/fbr.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import apexMarket from "../assets/img/apex_markets.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Apex Markets",
+      description: "Development",
+      imgUrl: apexMarket,
+      websiteUrl: "https://apexmarkets.io/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Home Shool panda",
+      description: "Design and Development",
+      imgUrl: hsp,
+      websiteUrl: "https://homeschoolpanda.com/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "NABR",
+      description: "Design and Development",
+      imgUrl: nabr,
+      websiteUrl: "https://www.nabr.org/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "FBR",
+      description: "Design and Development",
+      imgUrl: fbr,
+      websiteUrl: "https://fbresearch.org/",
+    },
+  ];
+
+  const projectsDiscription = [
+    {
+      title: "Apex Markets",
+      description:
+        "Apex Markets is a global market company that provides a secure, reliable, and convenient gateway into the world of open financial systems to aspiring traders ",
+      imgUrl: apexMarket,
+      websiteUrl: "https://apexmarkets.io/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Home Shool panda",
+      description:
+        "Homeschool Panda’s online homeschool planner lets you organize the lessons and saves valuable time. Moreover, this innovative digital planning record keeping & collaborative system can be accessed from anywhere and on any device.",
+      imgUrl: hsp,
+      websiteUrl: "https://homeschoolpanda.com/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "NABR",
+      description:
+        "This app advocates for ethical and essential animal research, ensuring sound public policy for the humane use of animals in biomedical research. It unifies the scientific community's voice on legislative, regulatory, and legal matters related to laboratory animal welfare.",
+      imgUrl: nabr,
+      websiteUrl: "https://www.nabr.org/",
+    },
+    {
+      title: "FBR",
+      description:
+        "This app facilitates the study and understanding of animals for scientific and medical discovery, spanning from wildlife observation to disease treatment development in laboratory animals and pets. It aims to advance cures by providing a platform for observational and theoretical research on animal behavior and biology.",
+      imgUrl: fbr,
+      websiteUrl: "https://fbresearch.org/",
     },
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <div>
+                    <h2>Projects</h2>
+                    <p>
+                      Welcome to my portfolio of projects! Below, you'll find a
+                      showcase of my recent work and accomplishments. Feel free
+                      to explore and get inspired!
+                    </p>
+                  </div>
+
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Projects</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Discription</Nav.Link>
+                      </Nav.Item>
+                      {/* <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item> */}
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {projectsDiscription.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      {/* <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane> */}
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
