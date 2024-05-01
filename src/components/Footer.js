e3f825ba6f71283fad2faf5751c5bae2645a370c
Mahad Ahmad <mahadahmad921@gmail.com>
2024-05-02 01:19:08 +0500

    update: footer section updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,51 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+      {/* <Container> */}
+        <Row style={{ paddingTop: 20 }} className="">
+          {/* <MailchimpForm /> */}
+          {/* <Col size={12} sm={6}>
+            <div
+              style={{
+                backgroundColor: "white",
+                borderRadius: "50%",
+                width: 80,
+                height: 80,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <span
+                style={{ color: "black", fontSize: 30, fontWeight: "bold" }}
+              >
+                MA
+              </span>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
+            <img src={logo} alt="Logo" />
+          </Col> */}
+          {/* <Col size={12} sm={6} > */}
+            <div className="social-icon text-center">
+              <a href="https://www.linkedin.com/in/-mahad/" target="_blank">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://www.github.com/Mahad-Ahmad" target="_blank">
+                <img src={navIcon2} alt="" />
+              </a>
+              {/* <a href="#">
+                <img src={navIcon3} alt="Icon" />
+              </a> */}
+            </div>
+          {/* </Col> */}
+          <p className="text-center">
+            Copyright {currentYear}. All Rights Reserved
+          </p>
         </Row>
-      </Container>
+      {/* </Container> */}
     </footer>
-  )
-}
+  );
+};
